50b80f215f074dc3ea7e5cddc36ccbfc445ab89d
Ukasz09 <246703@student.pwr.edu.pl>
2019-10-24 16:13:28 +0200

    tmp: delete all

--- README.md
+++
@@ -1,2 +0,0 @@
-# Valentine's Game
-Simple 2D game with special dedicate to my friend Kasia, who was inspiration for this project 
--- src/com/Ukasz09/ValentineGame/gameModules/effects/healthStatusBars/IHeartsPosition.java
+++
@@ -1,9 +0,0 @@
-package com.Ukasz09.ValentineGame.gameModules.effects.healthStatusBars;
-
-import com.Ukasz09.ValentineGame.gameModules.sprites.creatures.Creature;
-import javafx.geometry.Point2D;
-
-public interface IHeartsPosition {
-
-    Point2D calculateHeartCoords(Creature creature);
-}
